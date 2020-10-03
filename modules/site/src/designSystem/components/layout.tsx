5c08d1b4354d516cf50600aa94ae35d5d19859c4
Zoe Gagnon <zoe@zgagnon.com>
2020-10-03 14:40:56 -0400

    Center navlinks in their component
    - And make them 2 columns wide

--- modules/site/src/designSystem/components/layout.tsx
+++ modules/site/src/designSystem/components/layout.tsx
@@ -1,59 +1,83 @@
-import { Point } from "./typography"
+import React from "react"
 
-export const MAIN_GRID_COLUMNS = '75px 75px 75px 75px 75px 75px 75px 75px 75px 75px 75px 75px 75px 75px 75px  75px [end]'
-export const MAIN_GRID_ROWS = '50px 32px 32px auto  1px [bottom]'
+export const MAIN_GRID_COLUMNS =
+  "75px 75px 75px 75px 75px 75px 75px 75px 75px 75px 75px 75px 75px 75px 75px  75px [end]"
+export const MAIN_GRID_ROWS = "50px 32px 32px auto  1px [bottom]"
 
-export type Column = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 'end'
-export type Row = 1 | 2 | 3 | 4 | 5 | 'bottom'
+export type Column =
+  | 1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 11
+  | 12
+  | 13
+  | 14
+  | 15
+  | 16
+  | 17
+  | 18
+  | "end"
+export type Row = 1 | 2 | 3 | 4 | 5 | "bottom"
 
 export interface GridArea {
-	gridArea: [Row, Column, Row, Column]
+  gridArea: [Row, Column, Row, Column]
 }
 
-export const getGridArea = (gridArea: [Row, Column, Row, Column]) => `${gridArea[0]} / ${gridArea[1]} / ${gridArea[2]} / ${gridArea[3]}`
+export const getGridArea = (gridArea: [Row, Column, Row, Column]) =>
+  `${gridArea[0]} / ${gridArea[1]} / ${gridArea[2]} / ${gridArea[3]}`
 
 interface SectionProps {
-	title: string
+  title: string
 }
 
 export const Section: React.FC<SectionProps> = ({ children, title }) => {
-	return (
-		<div
-			css={{
-				display:       'flex',
-				flexDirection: 'column',
-				alignContent:  'space-around',
-				paddingBottom: '16px',
-			}}
-		>
-			<h2
-				css={{
-					display:        'flex',
-					justifyContent: 'left',
-					alignItems:     'center',
-					height:         '40px',
-					fontSize:       '32px',
-					lineHeight:     '40px',
-					fontFamily:     'Manjari',
-				}}
-			>
-				{title}
-			</h2>
-			{children}
-		</div>
-	)
+  return (
+    <div
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        alignContent: "space-around",
+        paddingBottom: "16px",
+      }}
+    >
+      <h2
+        css={{
+          display: "flex",
+          justifyContent: "left",
+          alignItems: "center",
+          height: "40px",
+          fontSize: "32px",
+          lineHeight: "40px",
+          fontFamily: "Manjari",
+        }}
+      >
+        {title}
+      </h2>
+      {children}
+    </div>
+  )
 }
 
-export const List:React.FC = ({children, className}) => {
-	return (
-		<ul
-			css={{
-				listStyleType:     "disc",
-				listStylePosition: "inside",
-			}}
-			className={className}
-		>
-			{children}
-		</ul>
-	)
+export const List: React.FC<{ className: string }> = ({
+  children,
+  className,
+}) => {
+  return (
+    <ul
+      css={{
+        listStyleType: "disc",
+        listStylePosition: "inside",
+      }}
+      className={className}
+    >
+      {children}
+    </ul>
+  )
 }
--- modules/site/src/layout/header.tsx
+++ modules/site/src/layout/header.tsx
@@ -1,80 +1,104 @@
 /** @jsx jsx */
-import {css, jsx} from '@emotion/core'
-import {Link} from "gatsby"
+import { css, jsx } from "@emotion/core"
+import { Link } from "gatsby"
 import React from "react"
-import colors from "../designSystem/colors";
-import {SecondaryLink} from '../designSystem/components/buttons'
-import {MAIN_GRID_COLUMNS} from '../designSystem/components/layout'
+import colors from "../designSystem/colors"
+import { SecondaryLink } from "../designSystem/components/buttons"
+import { MAIN_GRID_COLUMNS } from "../designSystem/components/layout"
 
 interface NavLinkProps {
-	column: number
-	width: number
-	to: string
+  column: number
+  width?: number
+  to: string
 }
 
-const NavLink: React.FC<NavLinkProps> = ({column, width = 1, to, children}) => {
-	return (
-		<Link to={to}
-		      css={{
-			      color: colors.darkShade,
-			      fontSize: '18px',
-			      fontFamily: 'Manjari',
-			      gridColumnStart: column,
-						gridColumnEnd: column + width,
-			      gridRowStart: 2,
-			      '@media print': {visibility: 'hidden'},
-		      }}>
-			{children}
-		</Link>
-	)
+const NavLink: React.FC<NavLinkProps> = ({
+  column,
+  width = 1,
+  to,
+  children,
+}) => {
+  return (
+    <Link
+      to={to}
+      css={{
+        textAlign: "center",
+        color: colors.darkShade,
+        fontSize: "18px",
+        fontFamily: "Manjari",
+        gridColumnStart: column,
+        gridColumnEnd: column + width,
+        gridRowStart: 2,
+        "@media print": { visibility: "hidden" },
+      }}
+    >
+      {children}
+    </Link>
+  )
 }
 
 const Header = (props: { siteTitle: string }) => (
-	<header
-		css={{
-			background: colors.brandColor,
-		}}>
-		<div
-			css={{
-				maxWidth: 1200,
-				margin: '0 auto',
-				display: 'grid',
-				paddingTop: 16,
-				gridTemplateColumns: MAIN_GRID_COLUMNS,
-				gridTemplateRows: '54px 18px [bottom]',
-				alignItems: 'center',
-			}}>
-			<h1
-				css={{
-					gridColumnStart: 1,
-					gridColumnEnd: 8,
-				}}>
-				<Link
-					to="/"
-					css={{
-						color: colors.darkShade,
-						textDecoration: `none`,
-						fontSize: '54px',
-						fontFamily: 'Manjari',
-					}}>
-					{
-						props.siteTitle
-					}
-				</Link>
-			</h1>
-			<NavLink to="/" column={1}> Home </NavLink>
-			<NavLink to="/resume" column={2}>Resume</NavLink>
-			<NavLink to="/manager-readme" column={3} width={2}>Manager Readme</NavLink>
-			<SecondaryLink href="mailto:zoe@zgagnon.com" gridArea={[2, 16, 'bottom', 'end']} reverseColors>Contact Me</SecondaryLink>
-		</div>
-		< div css={{
-			marginTop: 8,
-			border: `1px solid ${colors.lightAccent}`,
-			height: '4px',
-			background: colors.lightAccent,
-			pageBreakAfter: 'avoid',
-		}}/>
-	</header>
+  <header
+    css={{
+      background: colors.brandColor,
+    }}
+  >
+    <div
+      css={{
+        maxWidth: 1200,
+        margin: "0 auto",
+        display: "grid",
+        paddingTop: 16,
+        gridTemplateColumns: MAIN_GRID_COLUMNS,
+        gridTemplateRows: "54px 18px [bottom]",
+        alignItems: "center",
+      }}
+    >
+      <h1
+        css={{
+          gridColumnStart: 1,
+          gridColumnEnd: 8,
+        }}
+      >
+        <Link
+          to="/"
+          css={{
+            color: colors.darkShade,
+            textDecoration: `none`,
+            fontSize: "54px",
+            fontFamily: "Manjari",
+          }}
+        >
+          {props.siteTitle}
+        </Link>
+      </h1>
+      <NavLink to="/" column={1} width={2}>
+        Home
+      </NavLink>
+      <NavLink to="/resume" column={3} width={2}>
+        Resume
+      </NavLink>
+      <NavLink to="/manager-readme" column={5} width={2}>
+        Manager Readme
+      </NavLink>
+      <SecondaryLink
+        href="mailto:zoe@zgagnon.com"
+        gridArea={[2, 16, "bottom", "end"]}
+        reverseColors
+      >
+        Contact Me
+      </SecondaryLink>
+    </div>
+    <div
+      css={{
+        marginTop: 8,
+        border: `1px solid ${colors.lightAccent}`,
+        height: "4px",
+        background: colors.lightAccent,
+        pageBreakAfter: "avoid",
+      }}
+    />
+  </header>
 )
 
 export default Header
